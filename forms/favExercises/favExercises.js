dd6652e136a5e64bf7c4a927ae93512e92d65475
Laura <79476055+LGreenheck@users.noreply.github.com>
2021-03-29 16:48:19 -0500

    Update favExercises.js

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,7 +2,8 @@ var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+    exercises 
+    coreE
 }
 
 
