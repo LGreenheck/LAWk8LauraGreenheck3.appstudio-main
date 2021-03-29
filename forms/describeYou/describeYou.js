6294c46d5fd11afa7fbe226d29f736af6ff9e07e
Laura <79476055+LGreenheck@users.noreply.github.com>
2021-03-29 16:48:02 -0500

    Update describeYou.js

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,9 @@
+rdoPersonality.onchange=function(){
+ 
+ let userChoice = $("input[name=rdoPersonality]:checked").prop("value")
+  lblPersonality.value = `I would agree that you are a ${userChoice} person too!`
+}
 
-
-
-
-
+btnNextPage2.onclick=function(){
+ChangeForm(favExercises)
+}
