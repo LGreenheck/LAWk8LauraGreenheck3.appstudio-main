45cf30fb555441c57a3bf8c79bd1b07bfdefaa88
Laura <79476055+LGreenheck@users.noreply.github.com>
2021-04-02 18:04:36 -0500

    Update mobileNav.js

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -1,8 +1,22 @@
-
-hmbMenu.onclick=function(s){
-  if (typeof(s) == "object") 
-    return
-  /* add a switch here to take the user to the form
+hmbMenu.onclick = function(s) {
+    if (typeof(s) == "object")
+        return
+    /*  add a switch here to take the user to the form
      they chose. 
   */
+} else {
+    console.log(s)
+    switch (s) {
+        case "Dessert Voting":
+            ChangeForm(dessertVoting)
+            break
+        case "Describe You":
+            ChangeForm(describeYou)
+            break
+        case "Favorite Exercises":
+            ChangeForm(favExercises)
+            break
+        default:
+    }
+}
 }
