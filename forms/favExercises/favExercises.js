d721202c6e320d3e455bf680b46389b8fa178534
Laura <79476055+LGreenheck@users.noreply.github.com>
2021-04-02 17:59:14 -0500

    Update favExercises.js

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -1,11 +1,22 @@
-var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
-var coreExercises = ["situps","plank"]
+var exercises = ["pullups", "pushups", "situps", "jogging", "plank", "curls"]
+var coreExercises = ["situps", "plank"]
 
-favExercises.onshow=function(){
-    exercises 
-    coreE
+favExercises.onshow = function() {
+    for (i = 0; i < exercises.length; i++) {
+        selExercises.addItem(exercises[i])
+    }
 }
 
+selExercises.onfocusout = function() {
+        btnAboutChoices.onclick = function() {
+            if (selExercises.text[0] == "situps" && selExercises.text[1] == "plank") {
+                lblChoices.value = `You chose ${selExercises.text[0]} and ${selExercises.text[1]}.`
+            } else {
+                lblChoices.value = "You didn't pick two core exercises."
+            }
+        }
 
 
-
+        btnNextPage3.onclick = function() {
+            ChangeForm(mobileNav)
+        }
